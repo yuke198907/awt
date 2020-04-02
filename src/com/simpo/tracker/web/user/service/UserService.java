2cb6fdaa7b41f7ded8a3fb0547a52699bca7daa8
yuke <yuke198907@126.com>
2020-04-02 13:25:14 +0800

    用户管理

---
+++ src/com/simpo/tracker/web/user/service/UserService.java
@@ -0,0 +1,24 @@
+package com.simpo.tracker.web.user.service;
+
+import java.util.List;
+
+import com.simpo.tracker.web.user.entity.UserInfo;
+
+public interface UserService {
+	public UserInfo findInfoByLoginname(String loginname);
+	
+	//列表查询
+	public List<UserInfo> list(String pageNo, String pageSize, UserInfo info);
+	//条数查询
+	public int count(UserInfo info);
+	
+	public int add(UserInfo info);
+	
+	public int update(UserInfo info);
+	
+	public int updateStatus(long id, String status);
+	
+	public int updatePwd(long id, String pwd);
+	
+	public UserInfo findInfoById(long id);
+}
