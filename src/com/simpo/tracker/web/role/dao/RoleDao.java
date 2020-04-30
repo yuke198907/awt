00c18cdc310b7912a3acb382d5d40663faf1b5f2
yuke <yuke198907@126.com>
2020-04-30 09:33:37 +0800

    角色管理

---
+++ src/com/simpo/tracker/web/role/dao/RoleDao.java
@@ -0,0 +1,20 @@
+package com.simpo.tracker.web.role.dao;
+
+import java.util.List;
+
+import com.simpo.tracker.web.role.entity.RoleInfo;
+
+public interface RoleDao {
+	
+	public List<RoleInfo> list(RoleInfo info);
+	public int count(RoleInfo info);
+	
+	public RoleInfo findInfoById(long id);
+	
+	public void add(RoleInfo info) throws Exception;
+	
+	public void delete(long id) throws Exception;
+	
+	public void update(RoleInfo info) throws Exception;
+	
+}
---
+++ src/com/simpo/tracker/web/role/service/RoleService.java
@@ -0,0 +1,20 @@
+package com.simpo.tracker.web.role.service;
+
+import java.util.List;
+
+import com.simpo.tracker.web.role.entity.RoleInfo;
+
+public interface RoleService {
+	
+	public List<RoleInfo> list(boolean ispage, String pageNo, String pageSize, RoleInfo info);
+	public int count(RoleInfo info);
+	
+	public RoleInfo findInfoById(long id);
+	
+	public int add(RoleInfo info);
+	
+	public int delete(long id);
+	
+	public int update(RoleInfo info);
+	
+}
