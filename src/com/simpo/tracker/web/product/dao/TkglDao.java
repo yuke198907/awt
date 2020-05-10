7af5c31a19e3e56d82738439b4163a77a6f9f5b4
yuke <yuke198907@126.com>
2020-05-10 08:43:11 +0800

    图库管理dao

---
+++ src/com/simpo/tracker/web/product/dao/TkglDao.java
@@ -0,0 +1,19 @@
+package com.simpo.tracker.web.product.dao;
+
+import java.util.List;
+
+import com.simpo.tracker.web.product.entity.TkglInfo;
+
+public interface TkglDao {
+	
+	public List<TkglInfo> list(TkglInfo info);
+	public int count(TkglInfo info);
+	
+	public TkglInfo findInfoById(long id);
+	
+	public void add(TkglInfo info) throws Exception;
+	
+	public void delete(long id) throws Exception;
+	
+	public void updateVideo(TkglInfo info) throws Exception;
+}
