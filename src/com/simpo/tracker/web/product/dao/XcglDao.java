8a9ab408bd5311ba5dbb5f8ffe560c7d93c09745
yuke <yuke198907@126.com>
2020-05-22 07:53:51 +0800

    相册管理dao

---
+++ src/com/simpo/tracker/web/product/dao/XcglDao.java
@@ -0,0 +1,25 @@
+package com.simpo.tracker.web.product.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.simpo.tracker.web.product.entity.XcglInfo;
+
+public interface XcglDao {
+	
+	public List<XcglInfo> list(XcglInfo info);
+	public int count(XcglInfo info);
+	
+	public XcglInfo findInfoById(long id);
+	
+	public void add(XcglInfo info) throws Exception;
+	
+	public void delete(long id) throws Exception;
+	
+	public void update(XcglInfo info) throws Exception;
+	
+	public void updatePic(@Param("id")long id, @Param("pic")String pic) throws Exception;
+	
+	public void updateVideo(@Param("id")long id, @Param("video")String video) throws Exception;
+}
