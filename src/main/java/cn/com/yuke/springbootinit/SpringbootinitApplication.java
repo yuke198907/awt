92f4464ae7a1c4a8443b714648aa1df297bbb373
yuke <yuke198907@126.com>
2021-02-16 17:17:53 +0800

    增加启动类

--- src/main/java/cn/com/yuke/springbootinit/SpringbootinitApplication.java
+++ src/main/java/cn/com/yuke/springbootinit/SpringbootinitApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class SpringbootinitApplication {
 
     public static void main(String[] args) {
