fd165358e6c438fe3fdac2710f668fade84f7f62
yuke <yuke198907@126.com>
2021-02-16 17:18:13 +0800

    去除数据库配置

--- src/main/java/cn/com/yuke/springbootinit/SpringbootinitApplication.java
+++ src/main/java/cn/com/yuke/springbootinit/SpringbootinitApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SpringbootinitApplication {
 
     public static void main(String[] args) {
