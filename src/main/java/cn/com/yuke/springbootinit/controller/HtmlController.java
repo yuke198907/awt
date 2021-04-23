32a0ddf123c9c1b760db0cb64390604e4cf03f6f
yuke <yuke198907@126.com>
2021-04-23 13:27:41 +0800

    移除多余的类

--- src/main/java/cn/com/yuke/springbootinit/controller/HtmlController.java
+++ src/main/java/cn/com/yuke/springbootinit/controller/HtmlController.java
@@ -1,6 +1,5 @@
 package cn.com.yuke.springbootinit.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
