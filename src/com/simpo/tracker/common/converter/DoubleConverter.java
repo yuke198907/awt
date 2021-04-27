c91a39b8a19a9403c0bc1e1feba6f557c3e8425b
yuke <yuke198907@126.com>
2021-04-27 14:29:25 +0800

    2021-04-27:完善判断方式-yu

--- src/com/simpo/tracker/common/converter/DoubleConverter.java
+++ src/com/simpo/tracker/common/converter/DoubleConverter.java
@@ -1,5 +1,6 @@
 package com.simpo.tracker.common.converter;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.core.convert.converter.Converter;
 
 public class DoubleConverter implements Converter<String, Double> {
@@ -7,7 +8,7 @@ public class DoubleConverter implements Converter<String, Double> {
     @Override
     public Double convert(String param) {
         // TODO Auto-generated method stub
-        if (param != null && !"".equals(param)) {
+        if (!StringUtils.isBlank(param)) {
             return Double.valueOf(param);
         }
         return 0.0;
