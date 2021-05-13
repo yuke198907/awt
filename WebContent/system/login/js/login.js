568f2307f5f5357173f98d237facb261315df9ea
yuke <yuke198907@126.com>
2021-05-13 18:58:29 +0800

    2021-05-13:完善提示-yu

--- WebContent/system/login/js/login.js
+++ WebContent/system/login/js/login.js
@@ -7,7 +7,7 @@ function checkName() {
     var name = $('#username').val();
     if (name == null || name == "") {
         //提示错误
-        $('#count-msg').html("用户名不能为空");
+        $('#count-msg').html("用户名不能为空！");
         return false;
     }
     $('#count-msg').empty();
@@ -18,7 +18,7 @@ function checkPassword() {
     var password = $('#password').val();
     if (password == null || password == "") {
         //提示错误
-        $('#password-msg').html("密码不能为空");
+        $('#password-msg').html("密码不能为空！");
         return false;
     }
     $('#password-msg').empty();
