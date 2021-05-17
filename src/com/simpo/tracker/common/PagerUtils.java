6940e7d22e0b1e2926ab4505e099c4936e0180d0
yuke <yuke198907@126.com>
2021-05-17 20:38:15 +0800

    2021-05-17:分页Utils-yu

--- src/com/simpo/tracker/common/PagerUtils.java
+++ src/com/simpo/tracker/common/PagerUtils.java
@@ -1,5 +1,8 @@
 package com.simpo.tracker.common;
 
+/**
+ * 分页Utils
+ */
 public class PagerUtils {
     public final static String pageNo = "1";//分页---默认页
     public final static String pageSize = "50";//分页---默认分页条数
